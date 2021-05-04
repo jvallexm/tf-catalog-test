1568030696f9098bebc73c4acb2997eea34db83a
jvallexm <jvallexm@gmail.com>
2021-05-04 15:28:42 -0400

    TEST

---
+++ cos.tf
@@ -0,0 +1,124 @@
+##############################################################################
+# Terraform Providers
+##############################################################################
+
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "1.21.2"
+    }
+  }
+}
+
+##############################################################################
+
+
+##############################################################################
+# Account variables
+##############################################################################
+
+variable TF_VERSION {
+  default     = "0.13"
+  description = "The version of the Terraform engine that's used in the Schematics workspace."
+}
+
+variable ibmcloud_api_key {
+    description = "The IBM Cloud platform API key needed to deploy IAM enabled resources"
+    type        = string
+}
+
+variable ibm_region {
+    description = "IBM Cloud region where all resources will be deployed"
+    type        = string
+
+    validation  {
+      error_message = "Must use an IBM Cloud region. Use `ibmcloud regions` with the IBM Cloud CLI to see valid regions."
+      condition     = can(
+        contains([
+          "au-syd",
+          "jp-tok",
+          "eu-de",
+          "eu-gb",
+          "us-south",
+          "us-east"
+        ], var.ibm_region)
+      )
+    }
+}
+
+variable resource_group {
+    description = "Name of resource group where all infrastructure will be provisioned"
+    type        = string
+    default     = "asset-development"
+
+    validation  {
+      error_message = "Unique ID must begin and end with a letter and contain only letters, numbers, and - characters."
+      condition     = can(regex("^([a-z]|[a-z][-a-z0-9]*[a-z0-9])$", var.resource_group))
+    }
+}
+
+variable unique_id {
+    description = "A unique identifier need to provision resources. Must begin with a letter"
+    type        = string
+    default     = "asset-roks"
+
+    validation  {
+      error_message = "Unique ID must begin and end with a letter and contain only letters, numbers, and - characters."
+      condition     = can(regex("^([a-z]|[a-z][-a-z0-9]*[a-z0-9])$", var.unique_id))
+    }
+}
+
+
+##############################################################################
+
+
+##############################################################################
+# Resource Group
+##############################################################################
+
+data ibm_resource_group resource_group {
+  name = var.resource_group
+}
+
+##############################################################################
+
+
+##############################################################################
+# IBM Cloud Provider
+##############################################################################
+
+provider ibm {
+  ibmcloud_api_key      = var.ibmcloud_api_key
+  region                = var.ibm_region
+  generation            = 2
+  ibmcloud_timeout      = 60
+}
+
+##############################################################################
+
+
+##############################################################################
+# COS Instance
+##############################################################################
+
+resource ibm_resource_instance cos {
+  name              = "${var.unique_id}-cos"
+  service           = "cloud-object-storage"
+  plan              = "standard"
+  location          = "global"
+  resource_group_id = data.ibm_resource_group.resource_group.id
+
+  parameters = {
+    service-endpoints = "private"
+  }
+
+  timeouts {
+    create = "1h"
+    update = "1h"
+    delete = "1h"
+  }
+
+}
+
+##############################################################################
